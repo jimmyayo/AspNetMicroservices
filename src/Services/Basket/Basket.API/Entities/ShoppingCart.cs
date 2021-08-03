3ad117114268e0b4623ad4fccdbd4c80bf587321
jimmyayo <jimboyoo@gmail.com>
2021-08-02 19:00:49 -0700

    Catalog, Basket Microservices created

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+   public class ShoppingCart
+   {
+      public string UserName { get; set; }
+
+      public ShoppingCart(string userName)
+      {
+         UserName = userName;
+      }
+      public ShoppingCart() { }
+
+      public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+      public decimal TotalPrice
+      {
+         get
+         {
+            decimal totalprice = 0;
+            foreach (var item in Items)
+            {
+               totalprice += item.Price * item.Quantity;
+            }
+            return totalprice;
+         }
+      }
+   }
+
+}
