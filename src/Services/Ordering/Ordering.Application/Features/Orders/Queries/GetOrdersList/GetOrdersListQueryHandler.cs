82b1d17113fa2e5e053d1d3d5998dfb0bf92994c
jimmyayo <jimboyoo@gmail.com>
2021-08-08 14:53:01 -0700

    Add PipelineBehavior for Request validation, log unhandled errors

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -1,10 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Ordering.Application.Contracts.Persistence;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
